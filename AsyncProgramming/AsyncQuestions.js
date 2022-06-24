028b6da792f264fc4545259dd69a9d96c5269f31
MacMini <macmini@MacMinis-Mac-mini.local>
2022-06-25 00:11:59 +0530

    AsyncQuestions repo created

---
+++ AsyncProgramming/AsyncQuestions.js
@@ -0,0 +1,16 @@
+
+
+// Q1 -Print "Hello, world" with a delay of 3 seconds
+ 
+  function Q1(){
+
+    setTimeout(()=>{
+
+        console.log("Hello , world");
+        
+    },3000);
+
+  };
+
+  //Q2 -
+ 
---
+++ AsyncProgramming/note.md
@@ -0,0 +1,2 @@
+JavaScript challenges on Asynchronous programming - challenges
+
